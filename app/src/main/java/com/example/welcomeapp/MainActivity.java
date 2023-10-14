66c6601c9bc20fc802e206bc57c81345452dd08b
fury8 <ghozlanefaten@gmail.com>
2023-10-14 21:16:29 +0100

    Added Views
    Added a button , EditText & TextView

--- app/src/main/java/com/example/welcomeapp/MainActivity.java
+++ app/src/main/java/com/example/welcomeapp/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.welcomeapp;
 
+import static com.example.welcomeapp.R.*;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+
+        EditText myedttxt = findViewById(id.editTextTextPersonNameid);
+        String txtinput = myedttxt.getText().toString();
+
+        TextView mytextview = findViewById(R.id.txtVid);
+
+        Button mybttn = findViewById(id.btnid);
+        mybttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "Welcome to our app "+txtinput,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
